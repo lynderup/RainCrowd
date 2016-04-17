14500c984f8858171b0f5a9caa61c470f65031a2
Lynderup <lynderupjensen@gmail.com>
2016-04-17 16:28:48 +0200

    lookup of key istead of value store

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 var kad = require('kad');
 
-var dht = new kad.Node({
-  transport: kad.transports.UDP(kad.contacts.AddressPortContact({
+var rpc = kad.transports.UDP(kad.contacts.AddressPortContact({
     address: '127.0.0.1',
     port: 5000
-  })),
-  storage: kad.storage.FS('./test1/')
+}));
+
+var router = new kad.Router({
+    transport: rpc,
+    logger: new kad.Logger(3)
+})
+
+var dht = new kad.Node({
+    transport: rpc,
+    storage: kad.storage.FS('./test1/'),
+    router: router,
+    logger: new kad.Logger(3)
 });
 
 dht.on("join", () => {
-    console.log("Camilla er fuld");
+    router.findNode("1234", (err, contacts) => {
+	console.log(contacts);
+    });
 });
--- test.js
+++ test.js
@@ -1,33 +1,21 @@
 var kad = require('kad');
 
 var seed = {
-  address: '127.0.0.1',
-  port: 5000
+    address: '127.0.0.1',
+    port: 5000
 };
 
 var dht = new kad.Node({
-  transport: kad.transports.UDP(kad.contacts.AddressPortContact({
-    address: '127.0.0.1',
-    port: 5001
-  })),
-  storage: kad.storage.FS('./test2/')
+    transport: kad.transports.UDP(kad.contacts.AddressPortContact({
+	address: '127.0.0.1',
+	port: 5001
+    })),
+    storage: kad.storage.FS('./test2/'),
+    logger: new kad.Logger(3)
 });
 
-dht.connect(seed, (err) => {
-    
-  // dht.get(key, callback);
-  // dht.put(key, value, callback);
-});
+dht.connect(seed, (err) => {});
 
 
 dht.on("join", () => {
-    console.log("Hej Camilla");
-    dht.put("1234", "test", (err) => {
- 	console.log(err);
-	console.log(res);
-	dht.get("1234", (err, res) => {
-	    console.log(err);
-	    console.log(res);
-	};
-    });
 });

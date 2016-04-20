c5b9cd353f3de608d637447816c6a97b3d277f85
Lynderup <lynderupjensen@gmail.com>
2016-04-20 12:46:56 +0200

    refactored create network out of main

--- scr/main.js
+++ scr/main.js
@@ -1,24 +1,15 @@
-var kad = require('kad');
+var Kademlia = require('./kademlia').Kademlia;
 
-var rpc = kad.transports.UDP(kad.contacts.AddressPortContact({
-    address: '127.0.0.1',
-    port: 5000
-}));
+var args = process.argv.slice(2);
+var port = parseInt(args[0]);
 
-var router = new kad.Router({
-    transport: rpc,
-    logger: new kad.Logger(3)
-})
+var kad = new Kademlia("127.0.0.1", port);
 
-var dht = new kad.Node({
-    transport: rpc,
-    storage: kad.storage.FS('./test1/'),
-    router: router,
-    logger: new kad.Logger(3)
-});
-
-dht.on("join", () => {
-    router.findNode("1234", (err, contacts) => {
-	console.log(contacts);
-    });
-});
+if (args.length >= 3) {
+    var seed = {
+	address: args[1],
+	port: parseInt(args[2])
+    };
+    
+    kad.connect(seed);
+}

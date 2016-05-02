1e82b41c9bd03111266124041cf4f86fe44fddb2
Lukas Joergensen <lukaspj@outlook.com>
2016-05-02 17:51:22 +0200

    Added blockChain to main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 var Raindrop = require('./raindrop').Raindrop;
+var BlockChain = require('./blockChain').BlockChain;
 
+var blockChain = new BlockChain();
 var args = process.argv.slice(2);
 var port = parseInt(args[0]);
 
-var raindrop = new Raindrop("127.0.0.1", port, port + 1000);
+var raindrop = new Raindrop("127.0.0.1", port, port + 1000, blockChain);
 
 if (args.length >= 3) {
     var seed = {

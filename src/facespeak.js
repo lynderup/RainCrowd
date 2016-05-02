d1911cc29ffadbe16cf51e4af66e23f01b0fbae1
Lukas Joergensen <lukaspj@outlook.com>
2016-05-02 15:29:35 +0200

    Added random-program-generator

--- src/facespeak.js
+++ src/facespeak.js
@@ -29,7 +29,32 @@ var FaceSpeak = {
             return left / right;
         }
         throw "Invalid expression: " + program.expr;
+    },
+
+    generateRandom: (size) => {
+        if(size <= 0) return randomInt(100);
+        switch (randomInt(9)) {
+            case 0:
+                return randomInt(100);
+            case 1:
+            case 2:
+                return {expr: "plus", left: FaceSpeak.generateRandom(size - 1), right: FaceSpeak.generateRandom(size - 1)};
+            case 3:
+            case 4:
+                return {expr: "times", left: FaceSpeak.generateRandom(size - 1), right: FaceSpeak.generateRandom(size - 1)};
+            case 5:
+            case 6:
+                return {expr: "minus", left: FaceSpeak.generateRandom(size - 1), right: FaceSpeak.generateRandom(size - 1)};
+            case 7:
+            case 8:
+                return {expr: "divide", left: FaceSpeak.generateRandom(size - 1), right: FaceSpeak.generateRandom(size - 1)};
+        }
     }
 };
+
+function randomInt(rightBound)
+{
+    return Math.floor(Math.random() * rightBound);
+}
 
 exports.FaceSpeak = FaceSpeak;

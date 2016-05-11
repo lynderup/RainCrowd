056e7d96a4a9be03235c971ac8d2efbc7a5415b2
TheCamilla <camilla.m.v.f@gmail.com>
2016-05-11 14:12:17 +0200

    Reformatted interpreter test

--- src/tests/automatedTest/testInterpreter.js
+++ src/tests/automatedTest/testInterpreter.js
@@ -2,70 +2,70 @@ var FaceSpeak = require('./../../facespeak').FaceSpeak;
 var assert = require('assert');
 
 assert.equal(FaceSpeak.interpret({
+    expr: "plus",
+    left: {
+        expr: "times",
+        left: 3,
+        right: 3
+    },
+    right: 1
+}), 10);
+
+assert.equal(FaceSpeak.interpret({
+    expr: "if",
+    cond: 1,
+    body: 1,
+    else: 2
+}), 1);
+
+assert.equal(FaceSpeak.interpret({
+    expr: "if",
+    cond: 0,
+    body: 1,
+    else: 2
+}), 2);
+
+assert.equal(FaceSpeak.interpret({
+    expr: "if",
+    cond: true,
+    body: 1,
+    else: 2
+}), 1);
+
+assert.equal(FaceSpeak.interpret({
+    expr: "if",
+    cond: false,
+    body: 1,
+    else: 2
+}), 2);
+
+assert.equal(FaceSpeak.interpret({
+    expr: "let",
+    varname: "bar",
+    varexpr: 5,
+    body: {
         expr: "plus",
-        left: {
-            expr: "times",
-            left: 3,
-            right: 3
-        },
-        right: 1
-    }), 10);
+        left: 1,
+        right: "bar"
+    }
+}), 6);
+
 
 assert.equal(FaceSpeak.interpret({
-        expr: "if",
-        cond: 1,
-        body: 1,
-        else: 2
-    }), 1);
-
-assert.equal(FaceSpeak.interpret({
-        expr: "if",
-        cond: 0,
-        body: 1,
-        else: 2
-    }), 2);
-
-assert.equal(FaceSpeak.interpret({
-        expr: "if",
-        cond: true,
-        body: 1,
-        else: 2
-    }), 1);
-
-assert.equal(FaceSpeak.interpret({
-        expr: "if",
-        cond: false,
-        body: 1,
-        else: 2
-    }), 2);
-
-assert.equal(FaceSpeak.interpret({
-        expr: "let",
-        varname: "bar",
-        varexpr: 5,
-        body: {
-            expr: "plus",
-            left: 1,
-            right: "bar"
+    expr: "let",
+    varname: "foo",
+    varexpr: 5,
+    body: {
+        expr: "plus",
+        left: 1,
+        right: {
+            expr: "let",
+            varname: "foo",
+            varexpr: 2,
+            body: "foo"
         }
-    }), 6);
-
-
-assert.equal(FaceSpeak.interpret({
-        expr: "let",
-        varname: "foo",
-        varexpr: 5,
-        body: {
-            expr: "plus",
-            left: 1,
-            right: {
-                expr: "let",
-                varname: "foo",
-                varexpr: 2,
-                body: "foo"
-            }
-        }
-    }), 3);
+    }
+}), 3);
 
 assert.equal(FaceSpeak.interpret({
     expr: "for",

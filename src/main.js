52aaddc388fae30d70d9e99953667fbf2af4141e
Lukas Joergensen <lukaspj@outlook.com>
2016-05-18 16:05:34 +0200

    Now accepts host address in main.js

--- src/main.js
+++ src/main.js
@@ -3,15 +3,15 @@ var BlockChain = require('./blockChain').BlockChain;
 
 var blockChain = new BlockChain();
 var args = process.argv.slice(2);
-var port = parseInt(args[0]);
+var port = parseInt(args[1]);
 
-var raindrop = new Raindrop("0.0.0.0", port, port + 1000, blockChain);
+var raindrop = new Raindrop(args[0], port, port + 1000, blockChain);
 
-if (args.length >= 3) {
+if (args.length >= 4) {
     var seed = {
-        address: args[1],
-        port: parseInt(args[2]),
-        facePort: parseInt(args[2]) + 1000
+        address: args[2],
+        port: parseInt(args[3]),
+        facePort: parseInt(args[3]) + 1000
     };
     
     raindrop.connect(seed);

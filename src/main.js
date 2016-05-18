5dccff2b9c388b5a1fe7e87da742584769a346e0
Lukas Joergensen <lukaspj@outlook.com>
2016-05-18 14:48:51 +0200

    Changed local address from 127.0.0.1 to 0.0.0.0

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ var blockChain = new BlockChain();
 var args = process.argv.slice(2);
 var port = parseInt(args[0]);
 
-var raindrop = new Raindrop("127.0.0.1", port, port + 1000, blockChain);
+var raindrop = new Raindrop("0.0.0.0", port, port + 1000, blockChain);
 
 if (args.length >= 3) {
     var seed = {
